a8c579d3cba91f85613e53029a16bf0751074f61
MatheusMello95 <matheusamellom95@gmail.com>
2025-02-10 16:20:43 -0300

    Portifolio Rocketseat

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portfolio</title>
+  <script src="https://unpkg.com/@phosphor-icons/web@2.1.1"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+  <header class="bg-[url('./img/Background_Intro.png')] size-auto">
+    <div class="py-20">
+      <div class="justify-items-center">
+        <div class="py-10">
+          <img class="h-24 hover:animate-pulse rounded-full border-2 border-red-900" src="./img/avatar.svg" alt="Matheus Mello">
+        </div>
+        <div class="justify-items-center py-10 ">
+          <p class="text-red-50 ">Hello World! Meu nome é <span class="text-red-500">Matheus Mello</span> e sou </p>
+          <h1 class="text-5xl text-red-50 font-bold">Desenvolvedor PHP</h1>
+        </div>
+        <div class="text-center">
+          <p class="text-gray-400 text-sm">Transformo necessidades em aplicações reais, evolventes e funcionais. Desenvolvo sistemas através da minha paixão
+            pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos
+          </p>
+        </div>
+        <div class="flex space-x-4 justify-center py-10">
+          <span class="bg-lime-500 rounded-full px-2 py-1 font-semibold text-xs">GitHub</span>
+          <span class="bg-purple-500 rounded-full px-2 py-1 font-semibold text-xs">PHP</span>
+          <span class="bg-sky-500 rounded-full px-2 py-1 font-semibold text-xs">CSS</span>
+          <span class="bg-red-400 rounded-full px-2 py-1 font-semibold text-xs">HTML</span>
+          <span class="bg-amber-300 rounded-full px-2 py-1 font-semibold text-xs">Javascript</span>
+        </div>
+      </div>
+    </div>
+  </header>
+  <main class="justify-items-center bg-zinc-900">
+    <div class="justify-items-center">
+      <p><span class="text-red-400">Meu trabalho</span></p>
+      <h2 class="text-2xl text-white">Veja os projetos em destaque</h2>
+    </div>
+    <div class="grid grid-cols-2 gap-7 px-36 py-8">
+      <div class="flex bg-zinc-800 rounded-lg w-50% border border-zinc-800 hover:border-zinc-200">
+        <div class="w-1/3 px-3 py-3 ">
+          <img src="./img/image.png" alt="Social Midia" class="rounded">
+        </div>
+        <div class="w-2/3 px-4 py-4 relative">
+          <h1 class="font-semibold text-xl text-gray-300">Travelgram</h1>
+          <p class="text-gray-400">Rede social onde as pessoas mostram os registros das suas viagens pelo mundo</p>
+          <div class="gap-2 content-end absolute bottom-4">
+            <span class="bg-purple-500 rounded-full px-2 py-1 font-semibold text-xs">PHP</span>
+            <span class="bg-sky-500 rounded-full px-2 py-1 font-semibold text-xs">CSS</span>
+            <span class="bg-red-400 rounded-full px-2 py-1 font-semibold text-xs">HTML</span>
+            <span class="bg-amber-300 rounded-full px-2 py-1 font-semibold text-xs">Javascript</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex bg-zinc-800 rounded-lg w-50% border border-zinc-800 hover:border-zinc-200">
+        <div class="w-1/3 px-3 py-3 ">
+          <img src="./img/image-1.png" alt="Travel Image" class="rounded">
+        </div>
+        <div class="w-2/3 px-4 py-4 relative">
+          <h1 class="font-semibold text-xl text-gray-300">Pagina de Receita</h1>
+          <p class="text-gray-400">Pagina com o passo a passo de uma receita para cupcakes</p>
+          <div class="gap-2 content-end absolute bottom-4">
+            <span class="bg-purple-500 rounded-full px-2 py-1 font-semibold text-xs">PHP</span>
+            <span class="bg-sky-500 rounded-full px-2 py-1 font-semibold text-xs">CSS</span>
+            <span class="bg-red-400 rounded-full px-2 py-1 font-semibold text-xs">HTML</span>
+            <span class="bg-amber-300 rounded-full px-2 py-1 font-semibold text-xs">Javascript</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex bg-zinc-800 rounded-lg w-50% border border-zinc-800 hover:border-zinc-200">
+        <div class="w-1/3 px-3 py-3 ">
+          <img src="./img/image-2.png" alt="Travel Image" class="rounded">
+        </div>
+        <div class="w-2/3 px-4 py-4 relative">
+          <h1 class="font-semibold text-xl text-gray-300">Tech News</h1>
+          <p class="text-gray-400">Homepage de um portal de noticias sobre a area de tecnologia</p>
+          <div class="gap-2 content-end absolute bottom-4">
+            <span class="bg-purple-500 rounded-full px-2 py-1 font-semibold text-xs">PHP</span>
+            <span class="bg-sky-500 rounded-full px-2 py-1 font-semibold text-xs">CSS</span>
+            <span class="bg-red-400 rounded-full px-2 py-1 font-semibold text-xs">HTML</span>
+            <span class="bg-amber-300 rounded-full px-2 py-1 font-semibold text-xs">Javascript</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex bg-zinc-800 rounded-lg w-50% border border-zinc-800 hover:border-zinc-200">
+        <div class="w-1/3 px-3 py-3 ">
+          <img src="./img/Thumbnail_Project-04.png" alt="Travel Image" class="rounded">
+        </div>
+        <div class="w-2/3 px-4 py-4 relative">
+          <h1 class="font-semibold text-xl text-gray-300">Refund</h1>
+          <p class="text-gray-400">Um sistema para acompanhamento e pedido de reembols</p>
+          <div class="gap-2 content-end absolute bottom-4">
+            <span class="bg-purple-500 rounded-full px-2 py-1 font-semibold text-xs">PHP</span>
+            <span class="bg-sky-500 rounded-full px-2 py-1 font-semibold text-xs">CSS</span>
+            <span class="bg-red-400 rounded-full px-2 py-1 font-semibold text-xs">HTML</span>
+            <span class="bg-amber-300 rounded-full px-2 py-1 font-semibold text-xs">Javascript</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex bg-zinc-800 rounded-lg w-50% border border-zinc-800 hover:border-zinc-200">
+        <div class="w-1/3 px-3 py-3 ">
+          <img src="./img/Thumbnail_Project-05.png" alt="Travel Image" class="rounded">
+        </div>
+        <div class="w-2/3 px-4 py-4 relative">
+          <h1 class="font-semibold text-xl text-gray-300">Pagina de Turismo</h1>
+          <p class="text-gray-400">Pagina com as principais informações para quem quer viajar para Busan</p>
+          <div class="gap-2 content-end absolute bottom-4">
+            <span class="bg-purple-500 rounded-full px-2 py-1 font-semibold text-xs">PHP</span>
+            <span class="bg-sky-500 rounded-full px-2 py-1 font-semibold text-xs">CSS</span>
+            <span class="bg-red-400 rounded-full px-2 py-1 font-semibold text-xs">HTML</span>
+            <span class="bg-amber-300 rounded-full px-2 py-1 font-semibold text-xs">Javascript</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex bg-zinc-800 rounded-lg w-50% border border-zinc-800 hover:border-zinc-200">
+        <div class="w-1/3 px-3 py-3 ">
+          <img src="./img/Thumbnail_Project-06.png" alt="Travel Image" class="rounded">
+        </div>
+        <div class="w-2/3 px-4 py-4 relative">
+          <h1 class="font-semibold text-xl text-gray-300">Zingen</h1>
+          <p class="text-gray-400">Landing page completa e responsiva de um aplicativo de karaoke</p>
+          <div class="gap-2 content-end absolute bottom-4">
+            <span class="bg-purple-500 rounded-full px-2 py-1 font-semibold text-xs">PHP</span>
+            <span class="bg-sky-500 rounded-full px-2 py-1 font-semibold text-xs">CSS</span>
+            <span class="bg-red-400 rounded-full px-2 py-1 font-semibold text-xs">HTML</span>
+            <span class="bg-amber-300 rounded-full px-2 py-1 font-semibold text-xs">Javascript</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </main>
+  <footer class="bg-[url('./img/Background_Contacts.png')] size-auto justify-items-center">
+    <div class="py-10">
+      <div class="justify-items-center gap-4">
+        <div class="text-fuchsia-600 text-xl">Contato</div>
+        <div class="text-white text-xxl font-bold">Gostou do meu trabalho?</div>
+        <div class="text-white text-sm">Entre em contato ou acompanhe as minhas redes sociais!</div>
+      </div>
+      <div class="flex flex-col gap-7 py-2">
+        <div class="group flex justify-between items-center bg-zinc-800 w-96 h-[4rem] border border-zinc-800 hover:border-blue-400 gap-2 text-white rounded px-5">
+          <div class="flex items-center gap-2">
+            <i class="ph ph-linkedin-logo text-white group-hover:text-blue-400 text-2xl"></i>
+            Linkedin
+          </div>
+          <div>
+            <i class="ph ph-arrow-up-right text-xl" style="color:rgb(35, 124, 227) "></i>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-7 py-2">
+        <div class="group flex justify-between items-center bg-zinc-800 w-96 h-[4rem] border border-zinc-800 hover:border-blue-400 gap-2 text-white rounded px-5">
+          <div class="flex items-center gap-2">
+            <i class="ph ph-instagram-logo text-white group-hover:text-blue-400 text-2xl"></i>
+            Instagram
+          </div>
+          <div>
+            <i class="ph ph-arrow-up-right text-xl" style="color:rgb(35, 124, 227) "></i>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-7 py-2">
+        <div class="group flex justify-between items-center bg-zinc-800 w-96 h-[4rem] border border-zinc-800 hover:border-blue-400 gap-2 text-white rounded px-5">
+          <div class="flex items-center gap-2">
+            <i class="ph ph-github-logo text-white group-hover:text-blue-400 text-2xl"></i>
+            GitHub
+          </div>
+          <div>
+            <i class="ph ph-arrow-up-right text-xl" style="color:rgb(35, 124, 227) "></i>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-7 py-2">
+        <div class="group flex justify-between items-center bg-zinc-800 w-96 h-[4rem] border border-zinc-800 hover:border-blue-400 gap-2 text-white rounded px-5">
+          <div class="flex items-center gap-2">
+            <i class="ph ph-envelope-simple text-white group-hover:text-blue-400 text-2xl"></i>
+            E-mail
+          </div>
+          <div>
+            <i class="ph ph-arrow-up-right text-xl" style="color:rgb(35, 124, 227) "></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
